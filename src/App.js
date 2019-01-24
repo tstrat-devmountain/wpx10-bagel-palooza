987c6b3b9171a904bfc972fe3caf28d9d71d29a5
tstrat <tstrat@email.arizona.edu>
2019-01-24 13:38:17 -0700

    Adding in base files to work off off

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import BagelDisplay from './components/BagelDisplay/BagelDisplay';
+import BagelForm from './components/BagelForm/BagelForm';
 import './App.css';
 
 class App extends Component {
+  constructor(params) {
+    super(params);
+
+    this.state= {
+      bagels: [
+        {
+          name: 'Onion Bagel',
+          imageUrl:'http://cdn.shopify.com/s/files/1/1749/6693/products/onion-bagel_1024x1024.png?v=1531755332',
+          rating: 4
+        },
+        {
+          name: 'Blueberry Bagel',
+          imageUrl: 'http://cdn.shopify.com/s/files/1/1749/6693/products/blueberry_1024x1024.jpg?v=1531755323',
+          rating: 5
+        },
+        {
+          name: 'Wheat Bagel',
+          imageUrl: 'https://bagelbiz.com/wp-content/uploads/2017/11/Bagel-Biz-Whole-Wheat-Bagel.png',
+          rating: 2
+        },
+        {
+          name: 'Bantam Bagles',
+          imageUrl: 'https://hips.hearstapps.com/del.h-cdn.co/assets/16/41/1476297779-delish-bantam-bagels-2.jpg',
+          rating: 6
+        }
+      ]
+    }
+
+    this.addBagel = this.addBagel.bind(this);
+
+  }
+
+  addBagel(name, imageUrl, rating) {
+    const bagels = this.state.bagels;
+    bagels.push({ name, imageUrl, rating });
+    this.setState({
+      bagels
+    })
+  }
+
   render() {
+    const { bagels } = this.state;
+    const allTheBagels = bagels.map((bagel,i) =>
+        <BagelDisplay key={bagel.name+i}bagel={bagel}/>
+      )
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className='container'>
+          <h1>WpX10 Bagel Palooza</h1>
+          <div className='bagelContainer'>
+            {allTheBagels}
+          </div>
+        </div>
+        <div className='container'>
+          <h1>Add NEW Bagels</h1>
+          <BagelForm addBagel={this.addBagel}/>
+        </div>
       </div>
     );
   }
